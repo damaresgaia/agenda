248fb379659b37bb9f31a4021608b1e595210e95
Damares Gaia <damaresgaia@gmail.com>
2024-10-05 20:17:23 -0300

    CRUD Create

--- agenda/src/main/java/controller/Controller.java
+++ agenda/src/main/java/controller/Controller.java
@@ -1,17 +1,21 @@
 package controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import model.DAO;
 
-@WebServlet(urlPatterns = { "/Controller", "/main" })
+import model.DAO;
+import model.JavaBeans;
+
+@WebServlet(urlPatterns = { "/Controller", "/main", "/insert" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	DAO dao = new DAO();
+	JavaBeans contato = new JavaBeans();
 
 	public Controller() {
 		super();
@@ -23,6 +27,10 @@ public class Controller extends HttpServlet {
 		System.out.println(action);
 		if (action.equals("/main")) {
 			contatos(request, response);
+		} else if (action.equals("/insert")) {
+			novoContato(request, response);			
+		} else {
+			response.sendRedirect("index.html");
 		}
 	}
 
@@ -30,5 +38,14 @@ public class Controller extends HttpServlet {
 	protected void contatos(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.sendRedirect("agenda.jsp");
+	}
+
+	// Novo contato
+	protected void novoContato(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// setar as variáveis JavaBeans 
+		contato.setNome(request.getParameter("nome"));
+		contato.setFone(request.getParameter("fone"));
+		contato.setEmail(request.getParameter("email"));
 	}
 }
