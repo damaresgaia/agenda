4a9694419185b1da4dc6f929dc0c058c6c9e811a
damaresgaia <damaresgaia@gmail.com>
2024-10-11 09:46:25 -0300

    Configuração da classe dao e controller para editar Contato

--- agenda/src/main/java/controller/Controller.java
+++ agenda/src/main/java/controller/Controller.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import model.DAO;
 import model.JavaBeans;
 
-@WebServlet(urlPatterns = { "/Controller", "/main", "/insert" })
+@WebServlet(urlPatterns = { "/Controller", "/main", "/insert", "/select" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	DAO dao = new DAO();
@@ -30,7 +30,9 @@ public class Controller extends HttpServlet {
 		if (action.equals("/main")) {
 			contatos(request, response);
 		} else if (action.equals("/insert")) {
-			novoContato(request, response);			
+			novoContato(request, response);
+		} else if (action.equals("/select")) {
+			listarContato(request, response);
 		} else {
 			response.sendRedirect("index.html");
 		}
@@ -40,7 +42,7 @@ public class Controller extends HttpServlet {
 	protected void contatos(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// Criando um objeto que irá receber os dados JavaBeans
-		ArrayList<JavaBeans> lista = dao.listarContatos();	
+		ArrayList<JavaBeans> lista = dao.listarContatos();
 		// Encaminhar a lista ao documento agenda.jsp
 		request.setAttribute("contatos", lista);
 		RequestDispatcher rd = request.getRequestDispatcher("agenda.jsp");
@@ -50,7 +52,7 @@ public class Controller extends HttpServlet {
 	// Novo contato
 	protected void novoContato(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// setar as variáveis JavaBeans 
+		// setar as variáveis JavaBeans
 		contato.setNome(request.getParameter("nome"));
 		contato.setFone(request.getParameter("fone"));
 		contato.setEmail(request.getParameter("email"));
@@ -58,5 +60,15 @@ public class Controller extends HttpServlet {
 		dao.inserirContato(contato);
 		// redirecionar para o documento agenda.jsp
 		response.sendRedirect("main");
+	}
+
+	// Editar contato
+	protected void listarContato(HttpServletRequest request, HttpServletResponse response) {
+		// recebimento do id do contato que será editado
+		String idcon = request.getParameter("idcon");
+		// setar a variável JavaBeans
+		contato.setIdcon(idcon);
+		// Executar o método selecionarContato (DAO)
+		dao.selecionarContato(contato);			
 	}
 }
--- agenda/src/main/webapp/agenda.jsp
+++ agenda/src/main/webapp/agenda.jsp
@@ -23,6 +23,7 @@
 				<th>Nome</th>
 				<th>Fone</th>
 				<th>E-mail</th>
+				<th>Opções</th>
 			</tr>
 		</thead>
 		</tbody>
@@ -32,6 +33,7 @@
 			<td><%=lista.get(i).getNome() %></td>
 			<td><%=lista.get(i).getFone() %></td>
 			<td><%=lista.get(i).getEmail() %></td>
+			<td> <a href="select?idcon=<%=lista.get(i).getIdcon()%>" class="Botao1">Editar</a></td>
 		</tr>
 		<%} %>
 		</tbody>
